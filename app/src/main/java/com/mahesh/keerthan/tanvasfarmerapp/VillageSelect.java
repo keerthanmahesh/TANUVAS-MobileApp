d33fecfcc4e30e14740ebd6587e9419454a08973
Keerthan Mahesh <keerthan02@gmail.com>
2018-06-05 14:52:28 +0530

    Mark1

---
+++ app/src/main/java/com/mahesh/keerthan/tanvasfarmerapp/VillageSelect.java
@@ -0,0 +1,13 @@
+package com.mahesh.keerthan.tanvasfarmerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VillageSelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_village_select);
+    }
+}
