bfb0f080f5d4dbf3a85cf0d6c2d52be6621fd383
jainvandit <mailvandit.j@gmail.com>
2018-06-05 16:57:45 +0530

    #Mark3 Added OnClickListeners and Intents

--- app/src/main/java/com/mahesh/keerthan/tanvasfarmerapp/VillageSelect.java
+++ app/src/main/java/com/mahesh/keerthan/tanvasfarmerapp/VillageSelect.java
@@ -1,7 +1,10 @@
 package com.mahesh.keerthan.tanvasfarmerapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class VillageSelect extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class VillageSelect extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_village_select);
+        Button goButton = findViewById(R.id.goButton);
+        View.OnClickListener goButtonPressed = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               /* Intent villageSelected = new Intent(VillageSelect.this,HomeActivity.class);
+                startActivity(villageSelected); */
+            }
+        };
+        goButton.setOnClickListener(goButtonPressed);
     }
 }
