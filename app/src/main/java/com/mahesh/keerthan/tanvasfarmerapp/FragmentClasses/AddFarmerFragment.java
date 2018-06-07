cb7a20a3eea27267266304a24b8ac6b66034267f
Keerthan Mahesh <keerthan02@gmail.com>
2018-06-07 12:57:24 +0530

    Merge branch 'master' of https://github.com/keerthanmahesh/AppFarmer
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/res/layout/nav_header_home.xml

--- app/src/main/java/com/mahesh/keerthan/tanvasfarmerapp/FragmentClasses/AddFarmerFragment.java
+++ app/src/main/java/com/mahesh/keerthan/tanvasfarmerapp/FragmentClasses/AddFarmerFragment.java
@@ -7,15 +7,38 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.codetroopers.betterpickers.calendardatepicker.CalendarDatePickerDialogFragment;
 import com.mahesh.keerthan.tanvasfarmerapp.R;
 
+import java.util.Calendar;
 
-public class AddFarmerFragment extends Fragment {
+
+public class AddFarmerFragment extends Fragment implements CalendarDatePickerDialogFragment.OnDateSetListener, View.OnClickListener{
+
+    private View view;
+    private Button calender_button;
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.add_farmer_fragment,container,false);
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+            view = inflater.inflate(R.layout.add_farmer_fragment,container,false);
+            calender_button = (Button) view.findViewById(R.id.DOBbuttonAddfarmer);
+            calender_button.setOnClickListener(this);
+            return view;
+        }
+
+    @Override
+    public void onDateSet(CalendarDatePickerDialogFragment dialog, int year, int monthOfYear, int dayOfMonth) {
 
     }
+
+    @Override
+    public void onClick(View v) {
+        
+    }
 }
+
+
+
