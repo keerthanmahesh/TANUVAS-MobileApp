e41da689110671dc5b4ae04fa40c8c368372a6a1
jainvandit <mailvandit.j@gmail.com>
2018-06-22 00:47:37 +0530

    Added Profile Image Upload

---
+++ app/src/main/java/com/mahesh/keerthan/tanvasfarmerapp/RequestBuilder.java
@@ -0,0 +1,31 @@
+package com.mahesh.keerthan.tanvasfarmerapp;
+
+import okhttp3.FormBody;
+import okhttp3.HttpUrl;
+import okhttp3.RequestBody;
+
+public class RequestBuilder {
+    public static RequestBody LoginBody(String username, String password, String token) {
+         return new FormBody.Builder()
+                .add("action", "login")
+                .add("format", "json")
+                .add("username", username)
+                .add("password", password)
+                .add("logintoken", token)
+                .build();
+    }
+
+    public static HttpUrl buildURL(String path, String[] names,String[] values) {
+        HttpUrl.Builder builder =  new HttpUrl.Builder()
+                .scheme("http") //http
+                .host("192.168.1.45")
+                .addPathSegment("~vandit")
+                .addPathSegment(path)//adds "/pathSegment" at the end of hostname
+                .addQueryParameter(names[0], values[0]); //add query parameters to the URL
+
+        for(int i = 1;i<names.length;i++)
+            builder.addEncodedQueryParameter(names[i], values[i]);
+        return builder.build();
+
+    }
+}
