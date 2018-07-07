c744c44b8aab22a112f259e8d2c47bac1861a0e8
Keerthan Mahesh <keerthan02@gmail.com>
2018-07-07 17:20:36 +0530

    added onClickListener for livestockHoldingCardView

--- app/src/main/java/com/mahesh/keerthan/tanvasfarmerapp/FragmentClasses/QuestionFragment2.java
+++ app/src/main/java/com/mahesh/keerthan/tanvasfarmerapp/FragmentClasses/QuestionFragment2.java
@@ -1,7 +1,12 @@
 package com.mahesh.keerthan.tanvasfarmerapp.FragmentClasses;
 
+import android.content.Intent;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 import com.mahesh.keerthan.tanvasfarmerapp.R;
 
@@ -11,5 +16,20 @@ public class QuestionFragment2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_fragment2);
+        CardView cardView = findViewById(R.id.livestockholdingcardview);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animateIntentLivestockHolding(v);
+            }
+        });
+    }
+
+    private void animateIntentLivestockHolding(View v) {
+        Intent intent = new Intent(QuestionFragment2.this,QuestionFragment3.class);
+        String transitionName = "supguys";
+        View view_start = findViewById(R.id.livestockholdingcardview);
+        ActivityOptionsCompat optionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(QuestionFragment2.this,view_start,transitionName);
+        ActivityCompat.startActivity(QuestionFragment2.this,intent,optionsCompat.toBundle());
     }
 }
